14e3655f755cea1c96a8fbc2e4736703363109ea
Babitaadhikari11 <babeetaadhikari123@gmail.com>
2025-06-14 21:13:16 +0545

    absraction assignment

---
+++ AbstractionInJava/Vehicle.java
@@ -0,0 +1,37 @@
+package AbstractionInJava;
+
+abstract class Vehicles{
+    abstract void startEngine();
+    abstract void stopEngine();
+
+}
+class Car extends Vehicles{
+    @Override
+    void startEngine(){
+        System.out.println("Car's engine has started");
+    }
+    void stopEngine(){
+        System.out.println("Car has stopped");
+    }
+}
+class Motorcycle extends Vehicles{
+     @Override
+    void startEngine(){
+        System.out.println("Bike's engine has started");
+    }
+    void stopEngine(){
+        System.out.println("Motorcycle has stopped");
+    }
+}
+
+public class Vehicle {
+    public static void main(String[] args) {
+        Car corolla = new Car();
+        Motorcycle tvs = new Motorcycle();
+        corolla.startEngine();
+        tvs.startEngine();
+        corolla.stopEngine();
+        tvs.stopEngine();
+    }
+    
+}
